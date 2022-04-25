5e83eb5f8e2fb5a956cb084746c07f596be378ba
DaniilShmoylove <88559460+DaniilShmoylove@users.noreply.github.com>
2022-04-25 16:03:27 +0300

    Initial Commit

---
+++ Crypto-iOS/Crypto_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_iOSApp.swift
+//  Crypto-iOS
+//
+//  Created by Daniil Shmoylove on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
